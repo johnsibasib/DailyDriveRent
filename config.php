384ebe5905b0f07bb9ca90fd61d95c1a8d996c1c
johnsibasib <johnsibasib@gmail.com>
2022-05-22 19:47:04 +0800

    connection

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ ob_start(); //Turns on output buffering
 
 $timezone = date_default_timezone_set("Europe/London");
 
-$con = mysqli_connect("localhost", "root", "", "carrental"); //Connection variable
+$con = mysqli_connect("carrental.cg8zzvcrxjor.ap-southeast-1.rds.amazonaws.com", "admin", "Serwynstephen0987", "carrental"); //Connection variable
 
 if(mysqli_connect_errno()) 
 {
--- connection2.php
+++ connection2.php
@@ -1,3 +1,3 @@
 <?php 
-$con = mysqli_connect('localhost', 'root', '', 'carrental');
+$con = mysqli_connect('carrental.cg8zzvcrxjor.ap-southeast-1.rds.amazonaws.com', 'admin', 'Serwynstephen0987', 'carrental');
 ?>
